888ad6d9c7f5e8264b3e0e03cb815504b3d89ac6
Pepe García <pepe@pepegar.com>
2024-02-03 00:51:53 +0100

    init

---
+++ devenv.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  packages = [ pkgs.git ];
+
+  languages.nix.enable = true;
+  languages.haskell.enable = true;
+
+  pre-commit.hooks.deadnix.enable = true;
+  pre-commit.hooks.ormolu.enable = true;
+}
