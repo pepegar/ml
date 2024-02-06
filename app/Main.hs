d488e47f6569be95ea510d40f8560bba5fd28d14
Pepe García <pepe@pepegar.com>
2024-02-06 16:00:31 +0100

    adding value to intermediate nodes

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,24 @@
 module Main where
 
 import Data.Tree (drawTree)
-import ML
+import ML qualified
 import ML.Graph (forwardPass)
 
 main :: IO ()
 main = putStrLn result
   where
-    x1 :: Value
-    x1 = Value 2 "x1" 0
-    w1 :: Value
-    w1 = Value (-3) "w1" 0
-    x2 :: Value
-    x2 = Value 0 "x2" 0
-    w2 :: Value
-    w2 = Value 0 "w2" 0
-    b :: Value
-    b = Value 6.88137 "b" 0
+    x1 :: ML.Value
+    x1 = ML.Value 2 "x1" 0
+    w1 :: ML.Value
+    w1 = ML.Value (-3) "w1" 0
+    x2 :: ML.Value
+    x2 = ML.Value 0 "x2" 0
+    w2 :: ML.Value
+    w2 = ML.Value 0 "w2" 0
+    b :: ML.Value
+    b = ML.Value 6.88137 "b" 0
 
-    expr :: Value
+    expr :: ML.Value
     expr = tanh (x1 * w1 + x2 * w2 + b)
 
     result = drawTree (show <$> forwardPass expr)
--- lib/ML.hs
+++ lib/ML.hs
@@ -10,26 +10,26 @@ import Data.Functor.Foldable.TH (makeBaseFunctor)
 
 data Value
   = Value {_value :: Float, _name :: String, _grad :: Float}
-  | Add {_l :: Value, _r :: Value, _grad :: Float}
-  | Sub {_l :: Value, _r :: Value, _grad :: Float}
-  | Mul {_l :: Value, _r :: Value, _grad :: Float}
-  | Div {_l :: Value, _r :: Value, _grad :: Float}
-  | Abs {_val :: Value, _grad :: Float}
-  | Neg {_val :: Value, _grad :: Float}
-  | Exp {_val :: Value, _grad :: Float}
-  | Signum {_val :: Value, _grad :: Float}
-  | Relu {_val :: Value, _grad :: Float}
-  | Log {_val :: Value, _grad :: Float}
-  | Sin {_val :: Value, _grad :: Float}
-  | Cos {_val :: Value, _grad :: Float}
-  | Asin {_val :: Value, _grad :: Float}
-  | Acos {_val :: Value, _grad :: Float}
-  | Atan {_val :: Value, _grad :: Float}
-  | Sinh {_val :: Value, _grad :: Float}
-  | Cosh {_val :: Value, _grad :: Float}
-  | Asinh {_val :: Value, _grad :: Float}
-  | Acosh {_val :: Value, _grad :: Float}
-  | Atanh {_val :: Value, _grad :: Float}
+  | Add {_l :: Value, _r :: Value, _grad :: Float, _value :: Float}
+  | Sub {_l :: Value, _r :: Value, _grad :: Float, _value :: Float}
+  | Mul {_l :: Value, _r :: Value, _grad :: Float, _value :: Float}
+  | Div {_l :: Value, _r :: Value, _grad :: Float, _value :: Float}
+  | Abs {_val :: Value, _grad :: Float, _value :: Float}
+  | Neg {_val :: Value, _grad :: Float, _value :: Float}
+  | Exp {_val :: Value, _grad :: Float, _value :: Float}
+  | Signum {_val :: Value, _grad :: Float, _value :: Float}
+  | Relu {_val :: Value, _grad :: Float, _value :: Float}
+  | Log {_val :: Value, _grad :: Float, _value :: Float}
+  | Sin {_val :: Value, _grad :: Float, _value :: Float}
+  | Cos {_val :: Value, _grad :: Float, _value :: Float}
+  | Asin {_val :: Value, _grad :: Float, _value :: Float}
+  | Acos {_val :: Value, _grad :: Float, _value :: Float}
+  | Atan {_val :: Value, _grad :: Float, _value :: Float}
+  | Sinh {_val :: Value, _grad :: Float, _value :: Float}
+  | Cosh {_val :: Value, _grad :: Float, _value :: Float}
+  | Asinh {_val :: Value, _grad :: Float, _value :: Float}
+  | Acosh {_val :: Value, _grad :: Float, _value :: Float}
+  | Atanh {_val :: Value, _grad :: Float, _value :: Float}
   deriving (Show, Eq)
 
 $(makePrisms ''Value)
@@ -45,53 +45,79 @@ eval = cata go
   where
     go :: ValueF Float -> Float
     go (ValueF x _ _) = x
-    go (AddF x y _) = x + y
-    go (SubF x y _) = x - y
-    go (MulF x y _) = x * y
-    go (DivF x y _) = x / y
-    go (AbsF x _) = abs x
-    go (NegF x _) = negate x
-    go (ExpF x _) = exp x
-    go (SignumF x _) = signum x
-    go (ReluF x _) = max 0 x
-    go (LogF x _) = log x
-    go (SinF x _) = sin x
-    go (CosF x _) = cos x
-    go (AsinF x _) = asin x
-    go (AcosF x _) = acos x
-    go (AtanF x _) = atan x
-    go (SinhF x _) = sinh x
-    go (CoshF x _) = cosh x
-    go (AsinhF x _) = asinh x
-    go (AcoshF x _) = acosh x
-    go (AtanhF x _) = atanh x
+    go (AddF x y _ _) = x + y
+    go (SubF x y _ _) = x - y
+    go (MulF x y _ _) = x * y
+    go (DivF x y _ _) = x / y
+    go (AbsF x _ _) = abs x
+    go (NegF x _ _) = negate x
+    go (ExpF x _ _) = exp x
+    go (SignumF x _ _) = signum x
+    go (ReluF x _ _) = max 0 x
+    go (LogF x _ _) = log x
+    go (SinF x _ _) = sin x
+    go (CosF x _ _) = cos x
+    go (AsinF x _ _) = asin x
+    go (AcosF x _ _) = acos x
+    go (AtanF x _ _) = atan x
+    go (SinhF x _ _) = sinh x
+    go (CoshF x _ _) = cosh x
+    go (AsinhF x _ _) = asinh x
+    go (AcoshF x _ _) = acosh x
+    go (AtanhF x _ _) = atanh x
+
+forwardPass :: Value -> Value
+forwardPass = cata go
+  where
+    go :: ValueF Value -> Value
+    go (ValueF value name grad) = Value value name grad
+    go (AddF x y grad value) = Add x y grad $ eval (x + y)
+    go (SubF x y grad value) = Sub x y grad $ eval (x - y)
+    go (MulF x y grad value) = Mul x y grad $ eval (x * y)
+    go (DivF x y grad value) = Div x y grad $ eval (x / y)
+    go (AbsF x grad value) = Abs x grad $ eval (abs x)
+    go (NegF x grad value) = Neg x grad $ eval (negate x)
+    go (ExpF x grad value) = Exp x grad $ eval (exp x)
+    go (SignumF x grad value) = Signum x grad $ eval (signum x)
+    go (ReluF x grad value) = Relu x grad $ max 0 (eval x)
+    go (LogF x grad value) = Log x grad $ eval (log x)
+    go (SinF x grad value) = Sin x grad $ eval (sin x)
+    go (CosF x grad value) = Cos x grad $ eval (cos x)
+    go (AsinF x grad value) = Asin x grad $ eval (asin x)
+    go (AcosF x grad value) = Acos x grad $ eval (acos x)
+    go (AtanF x grad value) = Atan x grad $ eval (atan x)
+    go (SinhF x grad value) = Sinh x grad $ eval (sinh x)
+    go (CoshF x grad value) = Cosh x grad $ eval (cosh x)
+    go (AsinhF x grad value) = Asinh x grad $ eval (asinh x)
+    go (AcoshF x grad value) = Acosh x grad $ eval (acosh x)
+    go (AtanhF x grad value) = Atanh x grad $ eval (atanh x)
 
 instance Num Value where
-  x + y = Add x y 0
-  x * y = Mul x y 0
-  x - y = Sub x y 0
-  abs = flip Abs 0
-  signum = flip Signum 0
-  negate = flip Neg 0
+  x + y = Add x y 0 $ eval (x + y)
+  x * y = Mul x y 0 $ eval (x * y)
+  x - y = Sub x y 0 $ eval (x - y)
+  abs x = Abs x 0 $ eval x
+  signum x = Signum x 0 $ eval x
+  negate x = Neg x 0 $ eval x
   fromInteger x = Value (fromInteger x) "fromInteger" 1
 
 instance Fractional Value where
   fromRational :: Rational -> Value
   fromRational x = Value (fromRational x) "fromRational" 0
   (/) :: Value -> Value -> Value
-  x / y = Div x y 0
+  x / y = Div x y 0 $ eval (x / y)
 
 instance Floating Value where
   pi = Value pi "pi" 0
-  exp = flip Exp 0
-  log = flip Log 0
-  sin = flip Sin 0
-  cos = flip Cos 0
-  asin = flip Asin 0
-  acos = flip Acos 0
-  atan = flip Atan 0
-  sinh = flip Sinh 0
-  cosh = flip Cosh 0
-  asinh = flip Asinh 0
-  acosh = flip Acosh 0
-  atanh = flip Atanh 0
+  exp x = Exp x 0 $ eval $ exp x
+  log x = Log x 0 $ eval $ log x
+  sin x = Sin x 0 $ eval $ sin x
+  cos x = Cos x 0 $ eval $ cos x
+  asin x = Asin x 0 $ eval $ asin x
+  acos x = Acos x 0 $ eval $ acos x
+  atan x = Atan x 0 $ eval $ atan x
+  sinh x = Sinh x 0 $ eval $ sinh x
+  cosh x = Cosh x 0 $ eval $ cosh x
+  asinh x = Asinh x 0 $ eval $ asinh x
+  acosh x = Acosh x 0 $ eval $ acosh x
+  atanh x = Atanh x 0 $ eval $ atanh x
