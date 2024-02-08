18c1e729c7c48274a41b2fd72acd0816916e18c5
Pepe García <pepe@pepegar.com>
2024-02-08 16:31:58 +0100

    retain grads for non-leaf nodes

--- pythontest.py
+++ pythontest.py
@@ -10,20 +10,33 @@ w2 = torch.Tensor([1]).double()
 w2.requires_grad = True
 b = torch.Tensor([6.8813735870195432]).double()
 b.requires_grad = True
-
 x1w1 = x1 * w1
+x1w1.retain_grad()
 x2w2 = x2 * w2
+x2w2.retain_grad()
 x1w1x2w2 = x1w1 + x2w2
+x1w1x2w2.retain_grad()
 x1w1x2w2b = x1w1x2w2 + b
+x1w1x2w2b.retain_grad()
 coshx1w1x2w2b = x1w1x2w2b.cosh()
+coshx1w1x2w2b.retain_grad()
 sinhx1w1x2w2b = x1w1x2w2b.sinh()
+sinhx1w1x2w2b.retain_grad()
 tanhx1w1x2w2b = sinhx1w1x2w2b / coshx1w1x2w2b
+tanhx1w1x2w2b.retain_grad()
 
 tanhx1w1x2w2b.backward()
 
 print(tanhx1w1x2w2b.data.item())
-print(x1.grad.data)
-print(w1.grad.data)
-print(x2.grad.data)
-print(x2.grad.data)
-print(b.grad.data)
+print("x1", x1.grad.data)
+print("w1", w1.grad.data)
+print("x2", x2.grad.data)
+print("w2", w2.grad.data)
+print("b", b.grad.data)
+print("x1w1", x1w1.grad.data)
+print("x2w2", x2w2.grad.data)
+print("x1w1x2w2", x1w1x2w2.grad.data)
+print("x1w1x2w2b", x1w1x2w2b.grad.data)
+print("coshx1w1x2w2b", coshx1w1x2w2b.grad.data)
+print("sinhx1w1x2w2b", sinhx1w1x2w2b.grad.data)
+print("tanhx1w1x2w2b", tanhx1w1x2w2b.grad.data)
