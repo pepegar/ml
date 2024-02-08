1c90cf168df028d960e14ae8a4f7e44fdd3e400d
Pepe García <pepe@pepegar.com>
2024-02-08 16:20:09 +0100

    add a test file to compare against torch values

--- devenv.nix
+++ devenv.nix
@@ -1,11 +1,13 @@
 {pkgs, ...}: {
-  # https://devenv.sh/basics/
   packages = [
     pkgs.git
   ];
 
   languages.nix.enable = true;
   languages.haskell.enable = true;
+  languages.python.enable = true;
+  languages.python.poetry.enable = true;
+  languages.python.poetry.activate.enable = true;
 
   pre-commit.hooks.deadnix.enable = true;
   pre-commit.hooks.ormolu.enable = true;
---
+++ pythontest.py
@@ -0,0 +1,29 @@
+import torch
+
+x1 = torch.Tensor([2]).double()
+x1.requires_grad = True
+w1 = torch.Tensor([-3]).double()
+w1.requires_grad = True
+x2 = torch.Tensor([0]).double()
+x2.requires_grad = True
+w2 = torch.Tensor([1]).double()
+w2.requires_grad = True
+b = torch.Tensor([6.8813735870195432]).double()
+b.requires_grad = True
+
+x1w1 = x1 * w1
+x2w2 = x2 * w2
+x1w1x2w2 = x1w1 + x2w2
+x1w1x2w2b = x1w1x2w2 + b
+coshx1w1x2w2b = x1w1x2w2b.cosh()
+sinhx1w1x2w2b = x1w1x2w2b.sinh()
+tanhx1w1x2w2b = sinhx1w1x2w2b / coshx1w1x2w2b
+
+tanhx1w1x2w2b.backward()
+
+print(tanhx1w1x2w2b.data.item())
+print(x1.grad.data)
+print(w1.grad.data)
+print(x2.grad.data)
+print(x2.grad.data)
+print(b.grad.data)
