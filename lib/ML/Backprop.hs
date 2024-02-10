3ee38f2b9646bebb0604c97fba096f8c66371bea
Pepe García <pepe@pepegar.com>
2024-02-10 12:57:09 +0100

    more on backprop

--- lib/ML/Backprop.hs
+++ lib/ML/Backprop.hs
@@ -10,9 +10,9 @@ calculateGradients = flip (cata go) 1
     go :: ML.ValueF (Float -> ML.Value) -> Float -> ML.Value
     go (ML.ValueF input name _) = ML.Value input name
     go (ML.AddF l r grad value) = \n -> ML.Add (l n) (r n) (grad + n) value
-    go (ML.SubF l r grad value) = \n -> ML.Sub (l n) (r n) (grad + n) value
+    go (ML.SubF l r grad value) = \n -> ML.Sub (l n) (r (-n)) (grad + n) value
     go (ML.MulF l r grad value) = \n -> ML.Mul (l (r n ^. ML.value * n)) (r (l n ^. ML.value * n)) (grad + n) value
-    go (ML.DivF l r grad value) = \n -> ML.Div (l (n / r n ^. ML.value)) (r (negate (n * value / ((l n ^. ML.value) ** 2)))) (grad + n) value
+    go (ML.DivF l r grad value) = \n -> ML.Div (l (1 / r n ^. ML.value)) (r (-(l n ^. ML.value) / (r n ^. ML.value) ** 2)) (grad + n) value
     go (ML.AbsF input grad value) = \n -> ML.Abs (input n) (grad + n) value
     go (ML.NegF input grad value) = \n -> ML.Neg (input n) (grad + n) value
     go (ML.ExpF input grad value) = \n -> ML.Exp (input n) (grad + n) value
