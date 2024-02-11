ff0938b8cfd55438746cdf1070c7dd7755fe500b
Pepe García <pepe@pepegar.com>
2024-02-11 14:06:17 +0100

    seems the lib works with tensors now :)

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.Tensor qualified as T
 import Data.Tree (drawTree)
 import ML ((@=))
 import ML qualified
@@ -11,18 +12,18 @@ main = do
   putStrLn result
   print (ML.eval expr)
   where
-    x1 :: ML.Value Float
+    x1 :: ML.Value (T.Tensor '[3, 3] Float)
     x1 = "x1" @= 2
-    w1 :: ML.Value Float
+    w1 :: ML.Value (T.Tensor '[3, 3] Float)
     w1 = "w1" @= (-3)
-    x2 :: ML.Value Float
+    x2 :: ML.Value (T.Tensor '[3, 3] Float)
     x2 = "x2" @= 0
-    w2 :: ML.Value Float
+    w2 :: ML.Value (T.Tensor '[3, 3] Float)
     w2 = "w2" @= 1
-    b :: ML.Value Float
+    b :: ML.Value (T.Tensor '[3, 3] Float)
     b = "b" @= 6.8813735870195432
 
-    expr :: ML.Value Float
+    expr :: ML.Value (T.Tensor '[3, 3] Float)
     expr = ML.calculateGradients $ ML.forwardPass $ tanh (x1 * w1 + x2 * w2 + b)
 
     result = drawTree (show <$> toOp expr)
